8a804fdae5d9ed34d6b3d01e4d64af2831deafab
adrianarbosnavas <adrianarbosnavas@gmail.com>
2017-05-04 20:27:48 +0200

    Actualizo archivos :neckbeard:

--- DDBB/crearEsqueletoDDBB.sql
+++ DDBB/crearEsqueletoDDBB.sql
@@ -2,13 +2,15 @@ DROP DATABASE IF EXISTS warbo;
 
 CREATE DATABASE warbo;
 
+USE warbo;
+
 DROP TABLE IF EXISTS users;
 
 CREATE TABLE users (
 	name varchar(50),
-	password varchar(50),
+	password varchar(256),
     admin boolean
 );
 
-INSERT INTO users VALUES ('admin', 'admin', true);
-INSERT INTO users VALUES ('test', 'test', false);
+INSERT INTO users VALUES ('admin', SHA2('admin', 256), true);
+INSERT INTO users VALUES ('test', SHA2('test', 256), false);

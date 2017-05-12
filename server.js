02e2ba7915cced530ff21f7f15dc9925c179e451
HeasHeartfire <gagi1995@gmail.com>
2017-05-12 17:14:53 +0200

    Subir archivos

--- server.js
+++ server.js
@@ -168,6 +168,7 @@ var numUsers = 0;
 
 io.on('connection', function (socket) {
   var addedUser = false;
+  var players = [];
 
   // when the client emits 'new message', this listens and executes
   socket.on('new message', function (data) {
